df8cace81154ae3ea64e4462be011d60535a276d
Nil Aydinoz <aydinoznil@gmail.com>
2023-11-16 16:19:24 -0500

    adjusted syntax style

--- Movies.cpp
+++ Movies.cpp
@@ -4,13 +4,12 @@
 #include <vector>
 #include <sstream>
 #include <array>
-using namespace std;
 
 class Movie {
 private:
-    string title = "";
-    vector<string> genre = {};
-    vector<string> director = {};
+    std::string title = "";
+    std::vector<std::string> genre = {};
+    std::vector<std::string> director = {};
     int year = 0;
     int length = 0;
     float rating = 0.0;
@@ -18,10 +17,10 @@ private:
 public:
     //member functions and constructors
     Movie() {}
-    Movie(string title, vector<string>& genre, vector<string>& director, int year, int length, float rating, int numRatings);
+    Movie(std::string title, std::vector<std::string>& genre, std::vector<std::string>& director, int year, int length, float rating, int numRatings);
     // get functions
-    vector<string>& getGenre();
-    vector<string>& getDirector();
+    std::vector<std::string>& getGenre();
+    std::vector<std::string>& getDirector();
     int getLength();
     float getRating();
     int getNumRatings();
@@ -30,3 +29,64 @@ public:
     bool operator<(const Movie& movie1) const;
 
 };
+
+// constructor
+Movie::Movie(std::string title, std::vector<std::string>& genre, std::vector<std::string>& director, int year, int length, float rating, int numRatings) {
+    this->title = title;
+    this->genre = genre;
+    this->director = director;
+    this->year = year;
+    this->length = length;
+    this->rating = rating;
+    this->numRatings = numRatings;
+}
+
+// get functions
+std::vector<std::string>& Movie::getGenre() {
+    return this->genre;
+}
+std::vector<std::string>& Movie::getDirector() {
+    return this->director;
+}
+int Movie::getLength() {
+    return this->length;
+}
+float Movie::getRating() {
+    return this->rating;
+}
+int Movie::getNumRatings() {
+    return this->numRatings;
+}
+
+//// printing movie info
+//void Movie::print() {
+//    cout << this->title << endl;
+//    cout << "    Year: " << this->year << endl;
+//
+//    cout << "    Genre(s): ";
+//    // iterating through each genre (if necessary)
+//    for (string s : this->genre) {
+//        cout << s;
+//        if (s != genre.at(genre.size() - 1)) {
+//            cout << ", ";
+//        }
+//    }
+//    cout << endl;
+//
+//    cout << "    Director(s): ";
+//    // iterating through each director (if necessary)
+//    for (string s : this->director) {
+//        cout << s;
+//        if (s != director.at(director.size() - 1)) {
+//            cout << ", ";
+//        }
+//    }
+//    cout << endl;
+//
+//    cout << "    Rating: " << this->rating << " (Out of " << this->numRatings << ")" << endl;
+//    cout << "    Runtime: " << this->length << " minutes" << endl;
+//}
+
+bool Movie::operator<(const Movie& movie1) const {
+    return title < movie1.title;
+}
